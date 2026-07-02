dbd14ee57bc14415c962c8ae133874255cc5e02c
charlievellacott <charlie.vellacott@gmail.com>
2026-07-02 14:12:50 +0100

    alpaca, metrics and template nb

---
+++ performance/__init__.py
@@ -0,0 +1,23 @@
+from performance.metrics import (
+    calmar_ratio,
+    max_drawdown,
+    plot_rolling_metrics,
+    rolling_calmar_ratio,
+    rolling_max_drawdown,
+    rolling_sharpe_ratio,
+    rolling_sortino_ratio,
+    sharpe_ratio,
+    sortino_ratio,
+)
+
+__all__ = [
+    "calmar_ratio",
+    "max_drawdown",
+    "plot_rolling_metrics",
+    "rolling_calmar_ratio",
+    "rolling_max_drawdown",
+    "rolling_sharpe_ratio",
+    "rolling_sortino_ratio",
+    "sharpe_ratio",
+    "sortino_ratio",
+]
